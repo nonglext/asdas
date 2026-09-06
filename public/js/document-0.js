502a3556e5dcc45b5a1e649fef32a6e55ce526b5
nongl <nolukaga@gmail.com>
2026-09-07 00:51:16 +0300

    Delete public directory

--- public/js/document-0.js
+++
@@ -1,5 +0,0 @@
-// Не мигать экраном логина, если сессия уже есть
-    try {
-      if (localStorage.getItem('chatapp_token') && localStorage.getItem('chatapp_profile')) document.documentElement.classList.add('has-session');
-    } catch {}
-  
